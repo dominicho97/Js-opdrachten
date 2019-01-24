ad646cf5fe9ecdb9afb5fd576e23dee9f197da9a
dominicho97 <dominic.ho97@gmail.com>
2019-01-24 10:09:12 +0100

    tried , unfinished

---
+++ DOM/fixit/fixit2/script.js
@@ -0,0 +1,6 @@
+const title = document.getElementsByTagName(`h1`);
+const content = `BeCode The Beacon`;
+const title = title.textContent(content);
+document.getElementsByTagName(`h1`).classlist.add("shake");
+
+title.innerHTML("the crawling dead");
