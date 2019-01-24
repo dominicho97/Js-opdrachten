94febf720d19bcf243a517c5037f679043dfd4c4
dominicho97 <dominic.ho97@gmail.com>
2019-01-24 13:58:39 +0100

    added classlist, changed textContent

--- DOM/fixit/fixit2/script.js
+++ DOM/fixit/fixit2/script.js
@@ -1,4 +1,5 @@
 const title = document.getElementsByTagName(`h1`);
 const content = `The crawling dead`;
-const title = title.textContent(content);
-document.getElementsByTagName(`h1`).classlist.add("shake");
+title.textContent(content);
+
+document.getElementsByTagName(`h1`).classList.add("shake");
